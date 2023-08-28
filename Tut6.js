96dbeb0fb96092bf37ca724e1604dc4a7a659d1e
Mukul Gupta <98897351+DigiMukul@users.noreply.github.com>
2023-08-28 13:42:26 +0000

    container config

---
+++ Tut6.js
@@ -0,0 +1,10 @@
+// Data Type conversion confusion
+
+ let score = "15abc"
+
+ console.log(typeof score)
+ console.log(typeof(score))
+
+ let value = Number(score)// it will guaranted give you a number and you will do with other data types as well
+console.log(typeof value)
+
