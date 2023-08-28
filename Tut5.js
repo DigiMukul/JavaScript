89e42e6045a78b08a4ec95e9d4a1b6036b729759
Mukul Gupta <98897351+DigiMukul@users.noreply.github.com>
2023-08-28 12:28:30 +0000

    container config

---
+++ Tut5.js
@@ -0,0 +1,26 @@
+// Data Types and ECMA standards
+
+"use strict"; //treat all JS code as new version
+
+//alert(3+3) //it will not work in Node Js, it will work in browser
+
+ 
+
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+
+// object
+
+console.log(typeof "mukul");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+//interview question null and object ka type kya h
