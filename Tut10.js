839245eae6a9f134e262c5270ac96f7937e333e6
Mukul Gupta <98897351+DigiMukul@users.noreply.github.com>
2023-08-28 15:01:53 +0000

    container config

---
+++ Tut10.js
@@ -0,0 +1,8 @@
+// stack and heap memory 
+ 
+//Stack(Primitive data type m use hoti h)
+// stack memory jab bhi use hoti h to jo bhi hmne variable declare kia hota h hme uss variable ka copy milta h
+
+//Heap(Non - Primitive Data Type)
+// Heap memory use hoti h to hme reference milta h variable ka jo bhi change hoga to direct original variable m hoga
+
