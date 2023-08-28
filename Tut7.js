f2e0d5481f37688397a876a64da15dd4b384141c
Mukul Gupta <98897351+DigiMukul@users.noreply.github.com>
2023-08-28 14:02:53 +0000

    container config

---
+++ Tut7.js
@@ -0,0 +1,25 @@
+// Operations
+ let value = 3
+ let negVal = -value
+ console.log(negVal)
+
+ console.log(2+2);
+ console.log(2*2);
+ console.log(2*2);
+ console.log(2/2);
+ console.log(2**2);
+ console.log(2%2);
+
+ let str1 = "Hello"
+ let str2 = " Mukul"
+ console.log(str1 + str2);
+
+ console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+
+ console.log(true);// true
+ console.log(+true);// 1
+
+
